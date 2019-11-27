ac4352d6cd43b05753f4e8363b4d82f3b26a7c18
mrmlnc <mrmlnc@yandex-team.ru>
2019-11-27 19:49:22 +0300

    refactor(tests): simplify tests for completion provider

--- src/test/providers/completion.spec.ts
+++ src/test/providers/completion.spec.ts
@@ -2,11 +2,12 @@
 
 import * as assert from 'assert';
 
-import { CompletionItemKind } from 'vscode-languageserver';
+import { CompletionItemKind, CompletionList } from 'vscode-languageserver';
 
 import StorageService from '../../services/storage';
 import { doCompletion } from '../../providers/completion';
 import * as helpers from '../helpers';
+import { ISettings } from '../../types/settings';
 
 const storage = new StorageService();
 
@@ -29,114 +30,110 @@ storage.set('one.scss', {
 	imports: []
 });
 
+function getCompletionList(lines: string[], options?: Partial<ISettings>): CompletionList {
+	const text = lines.join('\n');
+
+	const settings = helpers.makeSettings(options);
+	const document = helpers.makeDocument(text);
+	const offset = text.indexOf('|');
+
+	return doCompletion(document, offset, settings, storage);
+}
+
 describe('Providers/Completion - Basic', () => {
 	it('Variables', () => {
-		const settings = helpers.makeSettings();
-		const document = helpers.makeDocument('$');
+		const actual = getCompletionList(['$|']);
 
-		assert.equal(doCompletion(document, 1, settings, storage).items.length, 5);
+		assert.equal(actual.items.length, 5);
 	});
 
 	it('Mixins', () => {
-		const settings = helpers.makeSettings();
-		const document = helpers.makeDocument('@include ');
+		const actual = getCompletionList(['@include |']);
 
-		assert.equal(doCompletion(document, 9, settings, storage).items.length, 1);
+		assert.equal(actual.items.length, 1);
 	});
 });
 
 describe('Providers/Completion - Context', () => {
 	it('Empty property value', () => {
-		const settings = helpers.makeSettings();
-		const document = helpers.makeDocument('.a { content:  }');
+		const actual = getCompletionList(['.a { content: | }']);
 
-		assert.equal(doCompletion(document, 14, settings, storage).items.length, 5);
+		assert.equal(actual.items.length, 5);
 	});
 
 	it('Non-empty property value without suggestions', () => {
-		const settings = helpers.makeSettings();
-		const document = helpers.makeDocument('.a { background: url(../images/one.png); }');
+		const actual = getCompletionList(['.a { background: url(../images/one|.png); }']);
 
-		assert.equal(doCompletion(document, 34, settings, storage).items.length, 0);
+		assert.equal(actual.items.length, 0);
 	});
 
 	it('Non-empty property value with Variables', () => {
-		const settings = helpers.makeSettings();
-		const document = helpers.makeDocument('.a { background: url(../images/#{$one}/one.png); }');
+		const actual = getCompletionList(['.a { background: url(../images/#{$one|}/one.png); }']);
 
-		assert.equal(doCompletion(document, 37, settings, storage).items.length, 5, 'True');
-		assert.equal(doCompletion(document, 42, settings, storage).items.length, 0, 'False');
+		assert.equal(actual.items.length, 5);
 	});
 
 	it('Discard suggestions inside quotes', () => {
-		const settings = helpers.makeSettings();
-		const document = helpers.makeDocument('.a { background: url("../images/#{$one}/$one.png"); @include test("test", $one); }');
+		const actual = getCompletionList([
+			'.a {',
+			'    background: url("../images/#{$one}/$one|.png");',
+			'}'
+		]);
 
-		assert.equal(doCompletion(document, 44, settings, storage).items.length, 0, 'Hide');
-		assert.equal(doCompletion(document, 38, settings, storage).items.length, 6, 'True');
-		assert.equal(doCompletion(document, 78, settings, storage).items.length, 5, 'Mixin');
+		assert.equal(actual.items.length, 0);
 	});
 
 	it('Custom value for `suggestFunctionsInStringContextAfterSymbols` option', () => {
-		const settings = helpers.makeSettings({
+		const actual = getCompletionList(['.a { background: url(../images/m|'], {
 			suggestFunctionsInStringContextAfterSymbols: '/'
 		});
-		const document = helpers.makeDocument('.a { background: url(../images/m');
 
-		assert.equal(doCompletion(document, 32, settings, storage).items.length, 1);
+		assert.equal(actual.items.length, 1);
 	});
 
 	it('Discard suggestions inside single-line comments', () => {
-		const settings = helpers.makeSettings();
-		const document = helpers.makeDocument('// $');
+		const actual = getCompletionList(['// $|']);
 
-		assert.equal(doCompletion(document, 4, settings, storage).items.length, 0);
+		assert.equal(actual.items.length, 0);
 	});
 
 	it('Discard suggestions inside block comments', () => {
-		const settings = helpers.makeSettings();
-		const document = helpers.makeDocument('/* $ */');
+		const actual = getCompletionList(['/* $| */']);
 
-		assert.equal(doCompletion(document, 4, settings, storage).items.length, 0);
+		assert.equal(actual.items.length, 0);
 	});
 
 	it('Identify color variables', () => {
-		const settings = helpers.makeSettings();
-		const document = helpers.makeDocument('$');
+		const actual = getCompletionList(['$|']);
 
-		const completion = doCompletion(document, 1, settings, storage);
-
-		assert.equal(completion.items[0].kind, CompletionItemKind.Variable);
-		assert.equal(completion.items[1].kind, CompletionItemKind.Variable);
-		assert.equal(completion.items[2].kind, CompletionItemKind.Color);
-		assert.equal(completion.items[3].kind, CompletionItemKind.Color);
-		assert.equal(completion.items[4].kind, CompletionItemKind.Color);
+		assert.equal(actual.items[0].kind, CompletionItemKind.Variable);
+		assert.equal(actual.items[1].kind, CompletionItemKind.Variable);
+		assert.equal(actual.items[2].kind, CompletionItemKind.Color);
+		assert.equal(actual.items[3].kind, CompletionItemKind.Color);
+		assert.equal(actual.items[4].kind, CompletionItemKind.Color);
 	});
 });
 
 describe('Providers/Completion - Implicitly', () => {
 	it('Show default implicitly label', () => {
-		const settings = helpers.makeSettings();
-		const document = helpers.makeDocument('$');
+		const actual = getCompletionList(['$|']);
 
-		assert.equal(doCompletion(document, 1, settings, storage).items[0].detail, '(implicitly) one.scss');
+		assert.equal(actual.items[0].detail, '(implicitly) one.scss');
 	});
 
 	it('Show custom implicitly label', () => {
-		const settings = helpers.makeSettings({
+		const actual = getCompletionList(['$|'], {
 			implicitlyLabel: '👻'
 		});
-		const document = helpers.makeDocument('$');
 
-		assert.equal(doCompletion(document, 1, settings, storage).items[0].detail, '👻 one.scss');
+		assert.equal(actual.items[0].detail, '👻 one.scss');
 	});
 
 	it('Hide implicitly label', () => {
-		const settings = helpers.makeSettings({
+		const actual = getCompletionList(['$|'], {
 			implicitlyLabel: null
 		});
-		const document = helpers.makeDocument('$');
 
-		assert.equal(doCompletion(document, 1, settings, storage).items[0].detail, 'one.scss');
+		assert.equal(actual.items[0].detail, 'one.scss');
 	});
 });
